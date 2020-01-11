bae2c91957c7adefccb638ad17cbd1c0e97171fa
dinganes <46746075+dinganes@users.noreply.github.com>
2020-01-11 13:58:03 +0530

    Have added few lines of java code

---
+++ FirstGitCode.java
@@ -0,0 +1,27 @@
+public class FirstGitCode {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Enter the number");
+
+		String read = br.readLine();
+
+		try {
+
+			int num = Integer.parseInt(read);
+
+			System.out.println(num + 10);
+
+		}
+
+		catch (NumberFormatException e) {
+
+			System.out.println("You should not give alpha values.......");
+		}
+
+		System.out.println("Welcome.........");
+
+	}
+}
